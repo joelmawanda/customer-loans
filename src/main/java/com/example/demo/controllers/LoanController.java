72aa962d8ce308ab6f146a4e2b14541ec9386b38
JOEL <JOELJ@stanbic.com>
2023-04-09 16:51:05 +0300

    add response code description for the api doc

--- src/main/java/com/example/demo/controllers/LoanController.java
+++ src/main/java/com/example/demo/controllers/LoanController.java
@@ -6,6 +6,9 @@ import com.example.demo.dtos.AuthRequest;
 import com.example.demo.dtos.LoanStatus;
 import com.example.demo.services.JwtService;
 import com.example.demo.services.LoanService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,12 +31,22 @@ public class LoanController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description="Successful operation"),
+            @ApiResponse(responseCode = "400", description="Bad Request. The request was invalid or cannot be served"),
+            @ApiResponse(responseCode = "401", description="Unauthorized. The authentication failed"),
+            @ApiResponse(responseCode = "403", description="Forbidden. The user may not be having the necessary permissions for a resource"),
+            @ApiResponse(responseCode = "404", description="Not Found. The resource could not be found"),
+            @ApiResponse(responseCode = "500", description="Internal server error. The server encountered an unexpected condition which prevented it fulfilling the request")
+    })
+    @Operation(summary = "Get a customer's loans by their account number", description = "Returns a list of all the customers loans")
     @GetMapping("/status")
     public ResponseEntity<?> getLoanStatus(@RequestParam("accountNumber") int accountNumber, @RequestHeader String Authorization) {
         LoanStatus loans = loanService.getLoanStatus(accountNumber);
         return new ResponseEntity<>(new OperationResponse(Constants.OPERATION_SUCCESS_CODE, Constants.OPERATION_SUCCESS_DESCRIPTION, "These are the customer's loans", loans), HttpStatus.OK);
     }
 
+    @Operation(summary = "Authenticate user", description = "Authenticates a user and generates a JWT token for the user")
     @PostMapping("/authenticate")
     public ResponseEntity<?> authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
         try {
