476e4d8e97929056754549c99d4c603af3dc1b7e
JOEL <JOELJ@stanbic.com>
2023-07-13 12:35:41 +0300

    add DTO

---
+++ src/main/java/com/example/demo/repositories/TokenRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.Customer;
+import com.example.demo.entities.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, String> {
+
+//    boolean findByUsername(String username);
+
+    Token findByUsername(String username);
+}
--- src/main/java/com/example/demo/services/JwtService.java
+++ src/main/java/com/example/demo/services/JwtService.java
@@ -1,21 +1,26 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Token;
+import com.example.demo.exceptions.InvalidAccountNumberException;
+import com.example.demo.exceptions.NoLoanFoundException;
+import com.example.demo.repositories.TokenRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 
 @Component
+@RequiredArgsConstructor
 public class JwtService {
 
+    private final TokenRepository tokenRepository;
 
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
@@ -50,10 +55,46 @@ public class JwtService {
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
+    public String generateToken(String userName) {
+        Token existingToken = tokenRepository.findByUsername(userName);
 
-    public String generateToken(String userName){
-        Map<String,Object> claims=new HashMap<>();
-        return createToken(claims,userName);
+        if (existingToken != null) {
+            Date expirationDate = extractExpirationDateFromToken(existingToken.getToken());
+
+            if (expirationDate != null && expirationDate.after(new Date())) {
+                return "You are already authenticated.";
+            }
+        }
+
+        Map<String, Object> claims = new HashMap<>();
+        String token = createToken(claims, userName);
+
+        // Store the token and its expiration date
+        Token newToken = new Token();
+        newToken.setUsername(extractUsernameFromToken(token));
+        newToken.setToken(token);
+        tokenRepository.save(newToken);
+        return token;
+    }
+
+    private Date extractExpirationDateFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getExpiration();
+    }
+
+    private String extractUsernameFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
     }
 
     private String createToken(Map<String, Object> claims, String userName) {
@@ -61,8 +102,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(userName)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-//                .setExpiration(new Date(System.currentTimeMillis()+1000*60*30)) //expires in 30 minutes
-                .setExpiration(new Date(System.currentTimeMillis()+(7*86400000L) + (12*3600000L))) //expires in one week and 12 hours
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*30)) //expires in 30 minutes
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
